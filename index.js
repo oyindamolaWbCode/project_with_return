c8ae8d1f4000e9bf300384689bb86e9bbdb0112f
oyindamolaWbCode <kushimo.oyindamola@gmail.com>
2022-06-23 00:52:19 +0100

    [UPDATE]

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const postsList = document.querySelector('.posts-list');
 const addPostForm = document.querySelector('.add-post-form');
+const titleValue = document.getElementById('title-value');
+const bodyValue = document.getElementById('body-value');
 
 let output = '';
 
@@ -37,4 +39,26 @@ const url = 'https://jsonplaceholder.typicode.com/posts'
 // Create - insert new post
 // Method :POST
 
-addPostForm.addEventListener
+addPostForm.addEventListener('submit', (e) =>{
+    e.preventDefault();
+
+    console.log(titleValue.value);
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            title:titleValue.value,
+            body:bodyValue.value
+        })
+    })
+
+    .then(response => response.json())
+    .then(data => {
+        const dataArr = [];
+        dataArr.push(data);
+        renderPosts(dataArr);
+    })
+})
