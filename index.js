59622dbc971e3d1745538dcc74f474153e8c6d23
oyindamolaWbCode <kushimo.oyindamola@gmail.com>
2022-06-22 23:46:24 +0100

    [NEW UPDATE] creating and getting the api

---
+++ index.js
@@ -0,0 +1,40 @@
+const postsList = document.querySelector('.posts-list');
+const addPostForm = document.querySelector('.add-post-form');
+
+let output = '';
+
+const renderPosts = (posts) =>{
+    // graps the data in the api
+    posts.forEach(post =>{
+        output += `
+        <div class="card mt-4 col-md-6 bg-ligt">
+        <div class="card-body">
+          <h5 class="card-title">${post.title}</h5>
+          <h6 class="card-subtitle mb-2 text-muted">${post.date}</h6>
+          <p class="card-text">${post.body}</p>
+          <a href="#" class="card-link">Edit</a>
+          <a href="#" class="card-link">Delete</a>
+        </div>
+      </div>
+        `;
+       
+     });
+      postsList.innerHTML = output
+
+}
+
+const url = 'https://jsonplaceholder.typicode.com/posts'
+// Get - Read the posts
+//method GET
+
+
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => renderPosts((data))
+       
+    )
+
+// Create - insert new post
+// Method :POST
+
+addPostForm.addEventListener
